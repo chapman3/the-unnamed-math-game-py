9ffe797b03da76cb1081916e71c076b2730a6bbe
chapman3 <Chapman3@uoregon.edu>
2016-03-18 22:34:16 -0700

    Create game.py

---
+++ game.py
@@ -0,0 +1,13 @@
+"""
+The Unnamed Math Game
+game.py
+
+Contains the main game logic functions for the unnamed math game.
+colored belt modes represent karate belts
+Functions:
+  [] init_numbers
+  [] init_white_belt
+  [] init_green_belt
+  [] init_red_belt
+  [] init_black_belt
+"""
