32da2640a66524c3865fc50dc46f641427ae8576
chapman3 <chapman3@uoregon.edu>
2016-03-19 19:32:38 -0700

    game_board constructor

--- game_board.py
+++ game_board.py
@@ -4,3 +4,11 @@ game_board.py
 
 class describing object game_board which is initialized after a difficulty is selected.
 """
+class Game_Board:
+
+    def __init__(self, rows, cols, ans):
+        self.rows = rows
+        self.cols = cols
+        self.ans = ans
+        return
+
